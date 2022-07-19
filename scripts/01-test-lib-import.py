490cefef0ee5b57f94b278893b6433d0f9d3a26f
Florian Golemo <fgolemo@gmail.com>
2022-07-19 12:40:20 -0400

    testing lib import

---
+++ scripts/01-test-lib-import.py
@@ -0,0 +1,11 @@
+import unitree_api_wrapper
+
+# ^ this should load the .so file
+
+import sys
+
+print(sys.version, sys.path)
+print("remember, this requires python 3.8")
+import robot_interface as sdk
+
+print("works")
---
+++ unitree_api_wrapper/__init__.py
@@ -0,0 +1,4 @@
+import sys
+
+# make library available internally
+sys.path.append("./lib/")
