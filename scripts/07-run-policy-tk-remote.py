ff89c0805046a5733c24d17efa704b13f2b3ff34
Florian Golemo <fgolemo@gmail.com>
2023-09-14 16:39:11 -0400

    adding log

--- scripts/07-run-policy-tk-remote.py
+++ scripts/07-run-policy-tk-remote.py
@@ -6,35 +6,41 @@ from unitree_api_wrapper.go1_controller import Go1Controller
 
 HISTORY = 1
 # controller = Go1Controller(policy_path="best_position.pt", obs_history=HISTORY, with_angvel=True)
-controller = Go1Controller(policy_path="lower_base.pt", obs_history=HISTORY, with_angvel=True)
-# controller = Go1Controller(policy_path="bool_lower.pt", obs_history=HISTORY, with_angvel=True, with_bool=True, bool_value=0)
+# controller = Go1Controller(policy_path="lower_base.pt", obs_history=HISTORY, with_angvel=True)
+controller = Go1Controller(policy_path="Sep13_21-43-55_s2r_test.pt", obs_history=HISTORY, with_angvel=True, with_bool=True, bool_value=0)
 
-controller.connect_and_stand()
+
 
 root = Tk()
 
 maxval = 0
 init_steps = 10
 step_counter = 0
+robot_is_up = False
 
 plotter = AsyncLivePlotter("real", 100, 0.25, with_angvel=True)
-# LOG_FOR = 1000
-# log_obs = []
-# log_act = []
+LOG_FOR = 1000
+log_obs = []
+log_act = []
 
 def step_env():
-    global obs, maxval, step_counter
+    global obs, maxval, step_counter, robot_is_up
+
+    if not robot_is_up:
+        controller.connect_and_stand()
+        robot_is_up = True
 
     slider_vals = [v.get() for v in sliders]
     cmd = torch.Tensor(slider_vals)
     state, obs, action = controller.control_highlevel(cmd)
     plotter.plot_stuff(obs[:, :, -1], action)
-    # log_obs.append(obs)
-    # log_act.append(action)
+    log_obs.append(obs.detach().cpu().numpy())
+    log_act.append(action)
 
-    # if len(log_obs) > LOG_FOR:
-    #     np.savez("real-log-2023-08-03.npz", obs = log_obs, act=log_act)
-    #     quit()
+    if len(log_obs) > LOG_FOR:
+        np.savez("s2r_test.npz", obs = log_obs, act=log_act)
+        print("saved file")
+        quit()
 
     root.after(int(controller.dt * 1000), step_env)  # reschedule event in 10 milliseconds
 
@@ -42,13 +48,13 @@ def step_env():
 sliders = []
 labels = ["pos_x", "pos_y", "orientation"]
 for label in labels:
-    slider = Scale(root, from_=-1, to=1, orient=HORIZONTAL, length=500, resolution=0.2, label=label)
+    slider = Scale(root, from_=-1.4, to=1.4, orient=HORIZONTAL, length=500, resolution=0.2, label=label)
     slider.set(0)
     slider.pack()
     sliders.append(slider)
 
 slider = Scale(root, from_=0.2, to=0.4, orient=HORIZONTAL, length=500, resolution=0.01, label="height")
-slider.set(0.25)
+slider.set(0.3)
 slider.pack()
 sliders.append(slider)
 
